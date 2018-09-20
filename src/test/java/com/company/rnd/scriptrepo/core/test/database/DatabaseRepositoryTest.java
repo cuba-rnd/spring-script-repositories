c4c3ccc31602beb3c49d2d5610ad0508e5028130
Andrey Belyaev <belyaev.andrey@gmail.com>
2018-09-20 20:34:57 +0300

    Implemented sample - executing script stored in DB #1

--- src/test/java/com/company/rnd/scriptrepo/core/test/database/DatabaseRepositoryTest.java
+++ src/test/java/com/company/rnd/scriptrepo/core/test/database/DatabaseRepositoryTest.java
@@ -1,7 +1,7 @@
 package com.company.rnd.scriptrepo.core.test.database;
 
 import com.company.rnd.scriptrepo.core.test.files.FileRepositoryTest;
-import com.company.rnd.scriptrepo.test.entity.PersistentScript;
+import org.hsqldb.jdbc.JDBCDataSource;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,28 +9,44 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Statement;
 
+@ContextConfiguration(locations = {"classpath:test-spring.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DatabaseRepositoryTest {
 
     private static final Logger log = LoggerFactory.getLogger(FileRepositoryTest.class);
 
-    private TestTaxService taxService;
-    private PersistentScript script;
+    @Autowired
+    private DatabaseRepositoryTestConfig.TestTaxService taxService;
+
+    @Autowired
+    private JDBCDataSource dataSource;
+
+    private Connection conn;
 
     @Before
     public void setUp() throws Exception {
-
-        script.setName("TestTaxCalculator.calculateTax");
-        script.setSourceText("return amount*0.13");
-
+        conn = dataSource.getConnection();
+        try (Statement st = conn.createStatement()){
+            st.execute("create table persistent_script (name varchar(255) not null, source_text varchar(1000) not null)");
+            st.execute("insert into persistent_script values('TestTaxCalculator.calculateTax', 'return amount*0.13')");
+        }
     }
 
     @After
     public void tearDown() throws Exception {
+        try (Statement st = conn.createStatement()){
+            st.execute("drop table persistent_script");
+            st.execute("shutdown");
+        }
+        conn.close();
     }
 
     @Test
