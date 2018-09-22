38c53e333bb35838ea346a490fe38fbfa1b19c58
Andrey Belyaev <belyaev.andrey@gmail.com>
2018-09-22 16:16:32 +0300

    Simplified XML config parsing using Spring's DomUtils #1

--- src/main/java/com/company/rnd/scriptrepo/repository/config/ScriptRepositoryConfigurationParser.java
+++ src/main/java/com/company/rnd/scriptrepo/repository/config/ScriptRepositoryConfigurationParser.java
@@ -9,11 +9,8 @@ import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
-import org.springframework.util.StringUtils;
+import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
@@ -56,17 +53,12 @@ public class ScriptRepositoryConfigurationParser implements BeanDefinitionParser
 
     private static List<String> getPackagesToScan(Element element){
         log.trace("Reading packages to be scanned to find Script Repositories");
-        NodeList elementsByTagName = element.getElementsByTagName(String.format("%sbase-packages", createPrefix(element)));
-        Node item = elementsByTagName.item(0);
-        NodeList basePackages =  item.getChildNodes();
-        int basePackagesCount = basePackages.getLength();
+        Element basePackagesEl = DomUtils.getChildElementByTagName(element, "base-packages");
+        List<Element> packageNames = DomUtils.getChildElementsByTagName(basePackagesEl, "base-package");
+        int basePackagesCount = packageNames.size();
         List<String> result = new ArrayList<>(basePackagesCount);
-        for (int i = 0; i < basePackagesCount; i++){
-            Node node = basePackages.item(i);
-            if (String.format("%sbase-package", createPrefix(element)).equals(node.getNodeName())) {
-                log.trace("Package found: {} content {}", node.getNodeName(), node.getTextContent());
-                result.add(node.getTextContent());
-            }
+        for (Element el : packageNames) {
+            result.add(el.getTextContent());
         }
         return result;
     }
@@ -75,30 +67,17 @@ public class ScriptRepositoryConfigurationParser implements BeanDefinitionParser
     private static Map<Class<? extends Annotation>, ScriptInfo> getCustomAnnotationsConfig(Element element) throws ClassNotFoundException {
         log.trace("Reading annotations configurations to create script methods later");
         Map<Class<? extends Annotation>, ScriptInfo> result = new HashMap<>();
-        NodeList elementsByTagName = element.getElementsByTagName(String.format("%sannotations-config", createPrefix(element)));
-        if (elementsByTagName == null || elementsByTagName.getLength() == 0){
+        Element annotConfigEl = DomUtils.getChildElementByTagName(element, "annotations-config");
+        if (annotConfigEl == null){
             return result;
         }
-        Node item = elementsByTagName.item(0);
-        NodeList configurations =  item.getChildNodes();
-        int configurationsCount = configurations.getLength();
-        for (int i = 0; i < configurationsCount; i++){
-            Node node = configurations.item(i);
-            if (String.format("%sannotation-mapping", createPrefix(element)).equals(node.getNodeName())) {
-                NamedNodeMap attributes = node.getAttributes();
-                log.trace("Annotation configuration found: {} annotation is {}", node.getNodeName(), attributes.getNamedItem("annotation-class"));
-                String providerBeanName = attributes.getNamedItem("provider-bean-name").getTextContent();
-                String executorBeanName = attributes.getNamedItem("executor-bean-name").getTextContent();
-                Class<? extends Annotation> annotationClass = (Class<? extends Annotation>)Class.forName(attributes.getNamedItem("annotation-class").getTextContent());
-                result.put(annotationClass, new ScriptInfo(annotationClass, providerBeanName, executorBeanName));
-            }
+        List<Element> annotConfig = DomUtils.getChildElementsByTagName(annotConfigEl, "annotation-mapping");
+        for (Element el : annotConfig) {
+            String providerBeanName = el.getAttribute("provider-bean-name");
+            String executorBeanName = el.getAttribute("executor-bean-name");
+            Class<? extends Annotation> annotationClass = (Class<? extends Annotation>)Class.forName(el.getAttribute("annotation-class"));
+            result.put(annotationClass, new ScriptInfo(annotationClass, providerBeanName, executorBeanName));
         }
         return result;
     }
-
-
-    private static String createPrefix(Element element) {
-        return StringUtils.isEmpty(element.getPrefix())?"":element.getPrefix()+":";
-    }
-
 }
